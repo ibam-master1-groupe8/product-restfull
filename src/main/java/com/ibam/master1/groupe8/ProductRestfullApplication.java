866571cf8643c7dbbb884286aeccecc022830b73
Osie <sielamoussaouattara@gmail.com>
2024-08-28 23:06:51 +0000

    initial project with database configuration

---
+++ src/main/java/com/ibam/master1/groupe8/ProductRestfullApplication.java
@@ -0,0 +1,13 @@
+package com.ibam.master1.groupe8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRestfullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibam/master1/groupe8/ProductRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibam.master1.groupe8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
